834ad922d5761c43c9570f7c1a01f00ac23c1390
oracle <denkosekka@gmail.com>
2013-12-18 19:39:37 +0900

    first

---
+++ output/MITI_docs-0.0.01/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Annai",
+        "MITI"
+    ],
+    "modules": [
+        "MITI",
+        "inazumatv"
+    ],
+    "allModules": [
+        {
+            "displayName": "inazumatv",
+            "name": "inazumatv"
+        },
+        {
+            "displayName": "MITI",
+            "name": "MITI"
+        }
+    ]
+} };
+});
